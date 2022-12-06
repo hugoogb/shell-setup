c457c329b693efe96bc07678449c98f477fb658e
Hugo GB <hugogaben8.02@gmail.com>
2022-12-06 06:31:01 +0100

    updated aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,18 +6,13 @@ alias distro="cat /etc/*-release | head -1 | tail -1 | cut -d= -f2"
 
 alias python="python3"
 
-# Custom aliases
 alias show="tree ."
 alias path="echo -e ${PATH//:/\\::}"
-
 
 # Network aliases
 alias myip="curl http://ipecho.net/plain; echo"
 alias speedtest="curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -"
 
-alias :q="exit"
-
-# alias
 alias dir='dir --color=auto'
 alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
@@ -37,11 +32,6 @@ alias catn='/bin/cat'
 alias catnl='bat --paging=never'
  
 alias ..='cd ..'
- 
-alias reload='source $HOME/.zshrc'
-
-alias :q='exit'
-alias :wq='exit'
 
 alias ghk='cat ~/.github-Pat'
 alias ghkc='catn ~/.github-Pat | head -3  | tail -1 | tr -d "\n" | clip.exe'
@@ -51,3 +41,6 @@ alias uni='cd /mnt/d/uni'
 alias unil='cd /mnt/c/uni'
 
 alias dev='cd /mnt/d/dev'
+
+alias :q="exit"
+alias :wq='exit'
